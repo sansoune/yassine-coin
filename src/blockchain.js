6be2ae56fe72cc8aa7810854a274f6e9e9ab57af
Yassine Dakhlaoui <75185439+sansoune@users.noreply.github.com>
2021-12-20 21:46:02 +0100

    adding the transacton

--- src/blockchain.js
+++ src/blockchain.js
@@ -12,6 +12,30 @@ class Blockchain {
 
         return block
     }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction)
+        this.AddBlock(transaction)
+        const newBlenceReciver = this.getBalence(transaction.toAdress) + transaction.amount
+        const newBalenceSender = this.getBalence(transaction.fromAdress) - transaction.amount
+        console.log(newBalenceSender)
+        this.AddBlock({adress: transaction.toAdress, balence: newBlenceReciver})
+        this.AddBlock({adress: transaction.fromAdress, balence: newBalenceSender})
+    }
+
+    getBalence(address) {
+        let balence = 0
+        const amount = this.chain.filter(adress => adress.data.adress === address)
+        if(amount[0] === undefined){
+             balence = 0
+             console.log('done')
+        }else {
+            const baleence = amount[amount.length-1].data.balence
+            balence = baleence
+            
+        }
+        return balence
+    }
 }
 
 
---
+++ src/transaction.js
@@ -0,0 +1,11 @@
+class Transaction {
+    constructor(fromAdress, toAdress, amount){
+        this.fromAdress = fromAdress
+        this.toAdress = toAdress
+        this.amount = amount
+    }
+    
+}
+
+
+module.exports = Transaction
