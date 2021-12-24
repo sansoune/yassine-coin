37bcdb4afad593671b32866fd02ac9fb5b03d39c
sansoune <yassine.dakhlaoui06@gmail.com>
2021-12-25 00:08:26 +0100

    nothing just for the look and organisation

--- test/script.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById('data').value
-const button = document.getElementById('submit')
-// const yassine_coin = require('./dev-test')
-
-button.onclick(() => {
-    yassine_coin.AddBlock(input)
-})
